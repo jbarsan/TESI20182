6f99d26bb1029f47e0dce8b61a14af9a12e64c88
João Carlos Barsan <joao_barsan@hotmail.com>
2018-09-20 21:32:03 -0300

    Aula 01

---
+++ Exemplo02.py
@@ -0,0 +1,13 @@
+import requests
+
+def download(url, num_retries=2):
+    print('Downloading:', url)
+    page = None
+    try:
+        response = requests.get(url)
+    except requestst.exceptions.RequestsException as e:
+        print('Download error:', e.reason)
+    return page
+
+page = download('http://www.google.com')
+print(page)
